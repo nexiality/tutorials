83193d1dfe7d145bf7522c55043043efd037d063
automike <mikeliucc@users.noreply.github.com>
2018-04-18 01:25:57 -0700

    test artifacts for database automation updated
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- artifact/data/rdbms-05-INSERT_new_customers.sql
+++ artifact/data/rdbms-05-INSERT_new_customers.sql
@@ -1,57 +1,16 @@
 INSERT INTO EMPLOYEES (EMPLOYEEID, LASTNAME, FIRSTNAME, TITLE, REPORTSTO, BIRTHDATE, ADDRESS, CITY, STATE, COUNTRY, POSTALCODE, EMAIL)
-	select
-		max(EMPLOYEEID) + 1,
-		'Brown',
-		'James',
-		'Funk Master',
-		'Nobody',
-		'1963-09-30',
-		'101 Beat Street',
-		'Funky Town',
-		'MI',
-		'USA',
-		'20931',
-		'funky@tac.com'
-	from EMPLOYEES
+select max(EMPLOYEEID) + 1, 'Brown', 'James', 'Funk Master', 'Nobody', '1963-09-30', '101 Beat Street', 'Funky Town', 'MI', 'USA', '20931', 'funky@tac.com' from EMPLOYEES
 ;
 
 INSERT INTO EMPLOYEES (EMPLOYEEID, LASTNAME, FIRSTNAME, TITLE, REPORTSTO, BIRTHDATE, ADDRESS, CITY, STATE, COUNTRY, POSTALCODE, EMAIL)
-	select
-		max(EMPLOYEEID) + 1,
-		'Marshall',
-		'Dean',
-		'Superstar',
-		'Hollywood',
-		'1961-11-02',
-		'900 Playhouse Crescent',
-		'Hollywood',
-		'MI',
-		'USA',
-		'900317',
-		'acting@easymoney.com'
-	from EMPLOYEES
-;
+select max(EMPLOYEEID) + 1, 'Marshall', 'Dean', 'Superstar', 'Hollywood', '1961-11-02', '900 Playhouse Crescent', 'Hollywood', 'MI', 'USA', '900317', 'acting@easymoney.com' from EMPLOYEES;
 
 INSERT INTO EMPLOYEES (EMPLOYEEID, LASTNAME, FIRSTNAME, TITLE, REPORTSTO, BIRTHDATE, ADDRESS, CITY, STATE, COUNTRY, POSTALCODE, EMAIL)
-	select
-		max(EMPLOYEEID) + 1,
-		'Stone',
-		'Rolling',
-		'Rockstars',
-		'No one',
-		'1965-03-24',
-		'1234 Fender Bender',
-		'Anytown',
-		'CA',
-		'USA',
-		'912345',
-		'amajor7@5thfret.mus'
-	from EMPLOYEES
+select max(EMPLOYEEID) + 1, 'Stone', 'Rolling', 'Rockstars', 'No one', '1965-03-24', '1234 Fender Bender', 'Anytown', 'CA', 'USA', '912345', 'amajor7@5thfret.mus' from EMPLOYEES
 ;
 
 -- nexial:new_employees
-SELECT *
-FROM EMPLOYEES WHERE LASTNAME in ('Brown', 'Marshall', 'Stone')
+SELECT * FROM EMPLOYEES WHERE LASTNAME in ('Brown', 'Marshall', 'Stone')
 ;
 
 
---
+++ artifact/data/rdbms-08.template2.sql
@@ -0,0 +1,1 @@
+INSERT INTO AVAILABLE_SONGS VALUES ('${artist name}', '${album}', '[TEXT(${song title}) => replace(','')]', '${unit price}', '$(sysdate|now|yyyy-MM-dd HH:mm:ss)');
