60b69d8c6e89cf99f29889d2dd29a8ad497146ff
automike <mikeliucc@users.noreply.github.com>
2019-02-20 02:27:31 -0800

    [general] - enhancement: minor improvement on logging to help troubleshoot test step errors.
    [macro commands]
    - [`description()`]: *NEW* no-op command for script author to provide macro documentation in a standard way.
    - [`expects(var,default)`]: *NEW* command to assert expected data variable prior to invoking the enclosing macro.
    - [`produces(var,value)`]: *NEW* command to indicate the data variable that will be created/overwritten as part of the enclosing macro execution.
    
    [web commands]
    - [`saveAttributeList(var,locator,attrName)`]: *NEW* command to save the value of an attribute for all matching elements.
    
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

---
+++ examples/macros/project-inspector.js
@@ -0,0 +1,31 @@
+let projectJson = {
+  "name": "macros",
+  "scanTime": 1550657505232,
+  "macros": [
+    {
+      "data": [
+        {
+          "description": "Inspect a Nexial commands page to collect the available commands (name and url) as a JSON array",
+          "expects": [
+            {
+              "description": "The URL for the target command type",
+              "name": "command-type.link.url",
+              "default": "(empty)"
+            }
+          ],
+          "produces": [
+            {
+              "description": "Data variable to combine command names and URL into JSON:\n1. combine command names and urls into CSV\n2. transpose so that we have a CSV of name and url columns\n3. convert to JSON array",
+              "name": "commands.json"
+            }
+          ],
+          "sheet": "macros",
+          "macro": "Inspect Command"
+        }
+      ],
+      "advices": [],
+      "location": "artifact/script",
+      "file": "MacroLibrary.xlsx"
+    }
+  ]
+};
