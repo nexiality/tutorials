f3acc19b80e920a1c1d94dc64a6c97f53f51f8f1
automike <mikeliucc@users.noreply.github.com>
2019-06-23 23:23:43 -0700

    [flow control] - [`EndLoopIf()`]: fix code to break repeat-until loop without breaking the enclosing iteration, and vice-versa.
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

---
+++ examples/failures/artifact/bin/ex9_iteration_macro_repeat_until_end_loop_if.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "--------------------------------------------------------------------------------"
+echo "[ EXAMPLE 9                                                                    ]"
+echo "--------------------------------------------------------------------------------"
+echo "When:"
+echo "  1. nexial.failFast is set to false"
+echo "  2. use iteration"
+echo "  3. scenario contains macro reference"
+echo "  4. referenced macro contains repeat-until"
+echo "  5. EndLoopIf() flow control activated when 2nd iteration and 3rd loop in repeat-until"
+echo "  6. EndLoopIf() flow control activated when 3rd iteration"
+echo "Then:"
+echo "  1. Nexial will abort the affected repeat-until loop (2nd iteration, 33rd loop bin u)"
+echo "  2. Nexial will continue with the remaining steps in referenced macro"
+echo "  3. Nexial will continue with the remaining scenarios"
+echo "  4. Nexial will continue to the next iteration"
+echo "  5. Nexial will continue abort 3rd iteration"
+echo "  6. Nexial will continue executing remaining iterations"
+echo "  7. Nexial will exit with a 0 (100% PASS). Early script termination DOES NOT constitutate any error condition"
+echo ""
+echo ""
+
+read -n 1 -s -r -p "Press any key to continue"
+
+nexial.sh -script ~/projects/nexial/tutorials/examples/failures/artifact/script/ScriptTest.xlsx -scenario Scenario5,Scenario3
