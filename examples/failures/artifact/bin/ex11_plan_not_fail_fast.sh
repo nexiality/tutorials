c2ccb07f119ae73e9ef4c995ffb76e5f6cfbe772
automike <mikeliucc@users.noreply.github.com>
2019-06-26 11:57:17 -0700

    done! added all fail fast and early termination examples.
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- examples/failures/artifact/bin/ex11_plan_not_fail_fast.sh
+++ examples/failures/artifact/bin/ex11_plan_not_fail_fast.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 echo "--------------------------------------------------------------------------------"
-echo "[ EXAMPLE 10                                                                   ]"
+echo "[ EXAMPLE 11                                                                   ]"
 echo "--------------------------------------------------------------------------------"
 echo "When:"
 echo "  1. nexial.failFast is set to true"
 echo "  2. use plan"
 echo "  3. first scenario of second script has a FAIL"
+echo "  4. 2nd plan test is set NOT_100 to fail fast"
 echo "Then:"
 echo "  1. Nexial will abort the affected scenario and script"
-echo "  2. Nexial will abort with the remaining plan since fail fail is set to true"
+echo "  1. Nexial will continue the subsequent plans"
 echo "  3. Nexial will exit with a -13 (NOT_100%_PASS_RATE)"
 echo ""
 echo ""
 
 read -n 1 -s -r -p "Press any key to continue"
 
-nexial.sh -plan ~/projects/nexial/tutorials/examples/failures/artifact/plan/PlanTest1.xlsx
+nexial.sh -plan ~/projects/nexial/tutorials/examples/failures/artifact/plan/PlanTest2.xlsx
--- examples/failures/artifact/bin/ex13_plan_iteration_endloopif.sh
+++ examples/failures/artifact/bin/ex13_plan_iteration_endloopif.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "--------------------------------------------------------------------------------"
-echo "[ EXAMPLE 10                                                                   ]"
+echo "[ EXAMPLE 13                                                                   ]"
 echo "--------------------------------------------------------------------------------"
 echo "When:"
 echo "  1. nexial.failFast is set to true"
@@ -9,11 +9,11 @@ echo "  2. use plan"
 echo "  3. first scenario of second script has a FAIL"
 echo "Then:"
 echo "  1. Nexial will abort the affected scenario and script"
-echo "  2. Nexial will abort with the remaining plan since fail fail is set to true"
+echo "  1. Nexial will continue the subsequent plans, asynchronously from the other plan steps"
 echo "  3. Nexial will exit with a -13 (NOT_100%_PASS_RATE)"
 echo ""
 echo ""
 
 read -n 1 -s -r -p "Press any key to continue"
 
-nexial.sh -plan ~/projects/nexial/tutorials/examples/failures/artifact/plan/PlanTest1.xlsx
+nexial.sh -plan ~/projects/nexial/tutorials/examples/failures/artifact/plan/PlanTest4.xlsx
