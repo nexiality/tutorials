81226975a3a0d124dc17441cb6d7f7a56cba7b22
automike <mikeliucc@users.noreply.github.com>
2020-02-20 08:12:22 -0800

    update example project for "Targeted Data Management"
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- examples/data-management/artifact/bin/my-nexial.sh
+++ examples/data-management/artifact/bin/my-nexial.sh
@@ -23,8 +23,11 @@ echo "$*"
 echo "BIN_HOME=$BIN_HOME"
 echo "PROJECT_HOME=$PROJECT_HOME"
 
-# check if requested project.properties exists
+PROJECT_PROP="$PROJECT_HOME/artifact/project.properties"
+LOCAL_PROP=$PROJECT_HOME/artifact/project.local.properties
 TARGET_PROP=$PROJECT_HOME/artifact/project.$1.properties
+
+# check if requested project.properties exists
 if [[ ! -f $TARGET_PROP ]]; then
   echo
   echo "ERROR: requested file $TARGET_PROP does not exists or is unreadable"
@@ -33,12 +36,23 @@ if [[ ! -f $TARGET_PROP ]]; then
 fi
 
 # backup existing project.properties, if found
+if [[ -f $PROJECT_PROP ]]; then
+  echo "moving existing project.properties to project.properties.BAK"
+  mv $PROJECT_PROP ${PROJECT_PROP}.BAK
+fi
 
 # combining local and requested project.properties
-
 # save combined properties to project.properties file
+if [[ "$LOCAL_PROP" == "$TARGET_PROP" ]]; then
+  echo "copy $LOCAL_PROP to $PROJECT_PROP"
+  cat "$TARGET_PROP" >"$PROJECT_PROP"
+else
+  echo "copy $LOCAL_PROP and $TARGET_PROP to $PROJECT_PROP"
+  cat "$LOCAL_PROP" "$TARGET_PROP" >"$PROJECT_PROP"
+fi
 
 # run nexial
+echo "running Nexial..."
 if [[ "$NEXIAL_HOME" == "" ]]; then
   NEXIAL_HOME=~/projects/nexial-core
 fi
