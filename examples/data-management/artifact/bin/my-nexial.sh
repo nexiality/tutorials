3569581e4cc33b6721370ee868f0a1b6d9fc306e
automike <mikeliucc@users.noreply.github.com>
2020-02-21 08:07:01 -0800

    [web] - support auto-search for chrome and firefox binary when the "default" or system-defined binary path cannot be found.
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- examples/data-management/artifact/bin/my-nexial.sh
+++ examples/data-management/artifact/bin/my-nexial.sh
@@ -10,18 +10,14 @@ if [[ $# -lt 3 ]]; then
 fi
 
 # find location of this script and this project
-BIN_HOME=$(
+PROJECT_BIN_HOME=$(
   cd "$(dirname "$0")" || exit
   pwd -P
 )
 PROJECT_HOME=$(
-  cd $BIN_HOME/../.. || exit
+  cd $PROJECT_BIN_HOME/../.. || exit
   pwd -P
 )
-
-echo "$*"
-echo "BIN_HOME=$BIN_HOME"
-echo "PROJECT_HOME=$PROJECT_HOME"
 
 PROJECT_PROP="$PROJECT_HOME/artifact/project.properties"
 LOCAL_PROP=$PROJECT_HOME/artifact/project.local.properties
